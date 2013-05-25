4cd8d96769e2387d6233c96d832318ceb95855db
bryanfoley <shagoff@gmail.com>
2013-05-25 23:35:35 +0200

    Update BFOL.h
    Added some basic typedefs

--- incl/BFOL.h
+++ incl/BFOL.h
@@ -8,4 +8,28 @@
 #ifndef BFOL_H_
 #define BFOL_H_
 
+//This is the return type from each function call
+typedef string RTN_MSG;
+
+//This is the type which represents an entire system of particles/grains/etc...
+typedef struct CONGLOMERATE
+{
+  2D_PARTICLE[N_MAX]
+}
+
+//This is the basic 2D particle type
+typedef struct 2D_PARTICLE
+{
+  double x,y;
+  double v_x, v_y;
+  double m,r,j;
+  int particleType;
+  double coordNumber;
+}
+
+//This is the basic 3D particle type
+typedef struct 3D_PARTICLE
+{
+  
+}
 #endif /* BFOL_H_ */
