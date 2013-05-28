33e4f1d9adab98babf399aa45df2014f14165382
bryanfoley <shagoff@gmail.com>
2013-05-28 18:15:03 +0300

    Create ErrorCodes.h
    The typedef of ERR_MSG is defined and implemented here for inclusion in all interfaces

---
+++ incl/ErrorCodes.h
@@ -0,0 +1,14 @@
+/*
+ * This is the return type from each function call
+ */
+typedef enum ERR_MSG
+{
+  ERR_OK,
+  ERR_OK_BUSY,
+  ERR_OK_FINISHED,
+	ERR_NOK,
+	ERR_NOK_FILE_NOT_FOUND,
+	ERR_NOK_OUT_OF_BOUNDS,
+	ERR_NOK_ENERGY_NOT_CONSERVED,
+	ERR_NOK_INCORRECT_TIMESTEP
+} ERR_MSG;
