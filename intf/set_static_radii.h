3bc18be460e86644cc20cbbf8e8e116c7c04d831
bryanfoley <shagoff@gmail.com>
2013-05-28 18:29:05 +0300

    Update set_static_radii.h
    First instance of the new return type: ERR_MSG

--- intf/set_static_radii.h
+++ intf/set_static_radii.h
@@ -8,6 +8,8 @@
 #ifndef SET_STATIC_RADII_H_
 #define SET_STATIC_RADII_H_
 
-void set_static_radii(double wall_particle[44][m_max][z_max], double rad){}
+3include"incl/errorCodes.h"
+
+ERR_MSG set_static_radii(double wall_particle[44][m_max][z_max], double rad){}
 
 #endif /* SET_STATIC_RADII_H_ */
